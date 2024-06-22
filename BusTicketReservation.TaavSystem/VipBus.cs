949469d0be1428d94028e1ba5ab516e4f06f17b8
naqdimuhammad <naqdimuhammad@gmail.com>
2024-06-22 18:10:40 +0330

    Update

--- BusTicketReservation.TaavSystem/VipBus.cs
+++ BusTicketReservation.TaavSystem/VipBus.cs
@@ -2,16 +2,17 @@ namespace BusTicketReservation.TaavSystem;
 
 public class VipBus : IReservable
 {
-    private static int counter = 1;
-    private const int capacity = 30;
+    private static int _counter = 1;
     
-    public int Id { get; } = counter++;
+    private const int Capacity = 30;
+    
+    public int Id { get; } = _counter++;
 
     public List<Seat> GetSeats()
     {
         List<Seat> seats = new List<Seat>();
 
-        for (int i = 1; i <= capacity; ++i)
+        for (int i = 1; i <= Capacity; ++i)
         {
             seats.Add(new Seat(i, SeatStatus.Free));
         }
